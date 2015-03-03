3d2f4fedf1e810f29754b30510addbb9ac73e319
scaussin <scaussin@student.42.fr>
2015-03-03 16:28:03 +0100

    ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: scaussin <scaussin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/20 11:58:13 by scaussin          #+#    #+#             */
-/*   Updated: 2015/03/02 19:38:14 by scaussin         ###   ########.fr       */
+/*   Updated: 2015/03/03 16:27:43 by scaussin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,16 @@ void	print(char c)
 
 void	print_header(t_header *header)
 {
-	//ft_printf("sizeof t_header : %d\nTINY_PAGE : %d\n", SIZE_H, TINY_PAGE);
+	ft_printf("sizeof t_header : %d\nTINY_PAGE : %d\n", SIZE_H, TINY_PAGE);
 	ft_printf("\n----------- print_header -----------");
+	if (!header)
+		ft_printf("\n(NULL)");
 	while (header)
 	{
-		ft_printf("\naddr : %p -> %d\nsize : %d\nreal size : %d\nfree : %d\nstring : %s\nnext : %p\n",
-			header, header, header->size, header->next ? (void *)header->next - (void *)header : (void *)header->next - (void *)header->next,
-			header->free, header + 1, header->next);
+		ft_printf("\naddr : %p -> %d\nsize : %d\nreal size : %d\nfree : %d\nstring : %s\nnext : %p -> %p\n",
+			header, header, 
+			header->size, header->next ? (void *)header->next - (void *)header : (void *)header->next - (void *)header->next,
+			header->free, header + 1, &(header->next), header->next);
 		header = header->next;
 		ft_printf("------------------------------------");
 	}
@@ -52,11 +55,32 @@ int			main()
 	//print_header(g_first_header.tiny);
 
 	char	*str2;
-	str2 = malloc2(sizeof(*str2) * 4096);
+	str2 = malloc2(sizeof(*str2) * 3000);
 	ft_printf("malloc 2 OK -> %p\n", str2);
-	ft_memset(str2, 'b', 4096);
-	str2[4095] = '\0';
+	ft_memset(str2, 'b', 3000);
+	str2[2999] = '\0';
+	// print_header(g_first_header.tiny);
+	char	*str3;
+	str3 = malloc2(sizeof(*str3) * 3000);
+	ft_printf("malloc 2 OK -> %p\n", str3);
+	ft_memset(str3, 'c', 3000);
+	str3[2999] = '\0';
 	//print_header(g_first_header.tiny);
+
+	char	*str4;
+	str4 = malloc2(sizeof(*str4) * 1000);
+	ft_printf("malloc 2 OK -> %p\n", str4);
+	ft_memset(str4, 'd', 1000);
+	str4[999] = '\0';
+
+
+	char	*str5;
+	str5 = malloc2(sizeof(*str5) * 30);
+	ft_printf("malloc 2 OK -> %p\n", str5);
+	ft_memset(str5, 'e', 30);
+	str5[29] = '\0';
+	print_header(g_first_header.tiny);
+
 	/*char	*str3;
 	if ((str3 = malloc2(sizeof(*str3) * 4096)))
 	{
@@ -96,6 +120,7 @@ void	*get_tiny(size_t size)//return first free
 {
 	t_header *tmp;
 	t_header *last;
+	t_header *tmptmp;
 
 	//print_header(g_first_header.tiny);
 	if (!g_first_header.tiny)
@@ -106,41 +131,42 @@ void	*get_tiny(size_t size)//return first free
 	tmp = g_first_header.tiny;
 	while (tmp)
 	{
-		ft_printf("while tmp\n");
-		if (tmp->free && tmp->size >= size + SIZE_H)
+		if (tmp->free && (tmp->size >= size + SIZE_H || (tmp->size >= size && tmp->next)))
 		{
 			if (tmp->next)
 			{
-				// TODO : raccordement
-				ft_printf("TODO : raccordement...");
-				return (0);
+				ft_printf("raccordement...\n");
+				tmp->free = 0;
+				if (tmp->size - size >= SIZE_H)
+				{
+					ft_printf("plus petit OK\n");
+					tmptmp = tmp->next;
+					tmp->next = (t_header *)((void *)tmp + size + SIZE_H);
+					tmp->next->size = tmp->size - size - SIZE_H;
+					tmp->next->free = 1;
+					tmp->next->prev = tmp;
+					tmp->next->next = tmptmp;
+					tmp->next->next->prev = tmp->next;
+					tmp->size = size;
+				}
 			}
 			else
 			{
 				tmp->free = 0;
 				tmp->next = (t_header *)((void *)tmp + size + SIZE_H);
-				/*if (tmp->size - SIZE_H == size)
-					tmp->next->size = 0;
-				else*/
-					tmp->next->size = tmp->size - size - SIZE_H;
+				tmp->next->size = tmp->size - size - SIZE_H;
 				tmp->size = size;
 				tmp->next->free = 1;
 				tmp->next->prev = tmp;
-				//print_header(g_first_header.tiny);
-				return (tmp + 1);
 			}
+			return (tmp + 1);
 		}
 		last = tmp;
 		tmp = tmp->next;
 	}
 	// liste pleine
-	ft_printf("avant - %p %p\n", &tmp, tmp);
-	print_header(tmp);
-
-	gen_tiny(&tmp);
-	ft_printf("apres - %p %p\n", &tmp, tmp);
-	print_header(tmp);
-	tmp->prev = last;
+	gen_tiny(&(last->next));
+	(void)last;
 	return(get_tiny(size));
 }
 
